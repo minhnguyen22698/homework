82439bcda65c6cbd724a1452de430109643e9ef7
minhnguyen22698 <minhnguyen22698@gmail.com>
2021-05-07 18:02:50 +0700

    update scroll view

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -1,4 +1,4 @@
-var valid = require('./validate')
+var valid = require('validate')
 cc.Class({
     extends: cc.Component,
 
@@ -20,7 +20,7 @@ cc.Class({
         },
         listView: {
             default: null,
-            type: cc.Node,
+            type: cc.ScrollView,
         },
         usernameLabel: cc.Label,
         passwordLabel: cc.Label,
@@ -30,12 +30,23 @@ cc.Class({
             default: null,
             type: cc.Prefab
         },
+        progressBar: {
+            default: null,
+            type: cc.ProgressBar,
+        },
+        labelEnough:{
+            default:null,
+            type:cc.RichText
+        },
         _password: '',
-        _username: ''
+        _username: '',
+        _index: 0
     },
     // use this for initialization
     onLoad: function () {
         this.usernameLabel.node.active = false;
+        this.listView.node.active = false;
+        this.labelEnough.node.active=false;
         this.passwordLabel.node.active = false;
         this.btnSubmit.node.on('click', this.onClick.bind(this, 'hahah'), this);
     },
@@ -44,21 +55,34 @@ cc.Class({
         var timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
         this.notification.string = `Chào mừng <b><color=fff000><u>${this._username}</u></color></b> đã gia nhập lúc <i><color=fcba03>${timestamp}</color></i>`
         var item = new cc.instantiate(this.listItem)
-        // item.width=64;
-        // item.height=64;
-        cc.log(this.listView)
-        this.listView.addChild(item)
-        
+        item.getComponent(cc.Label).string = this._username + ':' + timestamp;
+        item.y -= 30 * this._index;
+        this._index++;
+        this.progressBar.progress+=1/8
+        this.listView.node.active = true
+        cc.log(this.progressBar.progress)
+        if(this.progressBar.progress==1){
+            this.onEnough()
+        }
+        this.listView.content.addChild(item);
+
+    },
+    onEnough(){
+        this.username.node.active=false
+        this.password.node.active=false
+        this.labelEnough.node.active=true
     },
     onBeganUserName() {
         this.usernameLabel.string = 'Username cannot over 10 characters and includes special characters';
         this.usernameLabel.node.active = true;
         this.passwordLabel.node.active = false;
+        this.listView.node.active = false
     },
     onBeganPassword() {
         this.passwordLabel.string = 'Input password here and don`t forget it :3';
         this.usernameLabel.node.active = false;
         this.passwordLabel.node.active = true;
+        this.listView.node.active = false
     },
     onEditEndUN(arg) {
         var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
